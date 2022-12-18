0c86b87d68d24269ce3d45edd071a6965967c8d0
HuggieBo <triangl3@disroot.org>
2022-12-18 17:08:18 +0100

    working script which changes the broken line. Not tested on deck

--- install.sh
+++ install.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
-if [[ ! -f ~/Downloads/UHSC]]
+if [ -f ~/Downloads/UHSC ]
 then
-  mkdir ~/Downloads/UHSC
-else
-  rm -fr ~/Downloads/UHSC/
+rm -fr ~/Downloads/UHSC/
 fi
 
+
 # Move to Download folder and download latest release
-cd ~/Downloads
-curl -s https://api.github.com/repos/Bambosh/unofficial-homestuck-collection/releases/latest | grep "browser_download_url" | grep "tar.gz"| cut -d : -f 2,3| tr -d \" | wget -i -
+cd ~/Downloads || exit
+URL=$(curl -s https://api.github.com/repos/Bambosh/unofficial-homestuck-collection/releases/latest | grep "browser_download_url" | grep "tar.gz"| cut -d : -f 2,3| tr -d \")
+echo ${URL}
+wget --output-document=UHSC.tar.gz  ${URL}
+
 
 # Unpack
-tar -xf "*tar.gz" --directory=UHSC
-cd UHSC
-sed -i '7s/.*/  module.export = require(`..\/build\/Release\/sharp-linux-x64.node`);/ resources/app.asar.unpacked/node_modules/sharp/lib/sharp.js
+tar -xf UHSC.tar.gz --directory=UHSC || exit
+cd UHSC/* || exit
+sed -i '7s/.*/  module.export = require(`..\/build\/Release\/sharp-linux-x64.node`);/' resources/app.asar.unpacked/node_modules/sharp/lib/sharp.js
 
 
